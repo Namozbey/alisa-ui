a585db16ded9fb0f361c4d49b0c81df9570110b6
Namozbey <nostonayev@gmail.com>
2021-11-09 12:26:43 +0500

    test(src): add new test for each level of colors (50 - 900)

--- src/components/colors/__tests__/ColorBox.jsx
+++ src/components/colors/__tests__/ColorBox.jsx
@@ -19,8 +19,62 @@ const setup = (testProps = {}) => {
 }
 
 describe('ColorBox', () => {
-  it('renders matching snapshot', () => {
+  it('renders matching snapshot for color level 50', () => {
+    const { wrapper } = setup({ color: 'teal-50' })
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders matching snapshot for color level 100', () => {
+    const { wrapper } = setup({ color: 'teal-100' })
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders matching snapshot for color level 200', () => {
+    const { wrapper } = setup({ color: 'teal-200' })
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders matching snapshot for color level 300', () => {
+    const { wrapper } = setup({ color: 'teal-300' })
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders matching snapshot for color level 400', () => {
+    const { wrapper } = setup({ color: 'teal-400' })
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders matching snapshot for color level 500', () => {
     const { wrapper } = setup({ color: 'teal-500' })
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders matching snapshot for color level 600', () => {
+    const { wrapper } = setup({ color: 'teal-600' })
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders matching snapshot for color level 700', () => {
+    const { wrapper } = setup({ color: 'teal-700' })
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders matching snapshot for color level 800', () => {
+    const { wrapper } = setup({ color: 'teal-800' })
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders matching snapshot for color level 900', () => {
+    const { wrapper } = setup({ color: 'teal-900' })
 
     expect(wrapper).toMatchSnapshot()
   })
